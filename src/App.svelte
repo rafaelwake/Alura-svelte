20d919c49228ec0093a062bbafdceebe7039e953
Rafael Bernardes <60991402+rafaelwake@users.noreply.github.com>
2022-09-01 23:27:09 -0300

    Curso-3-Next-CriandoInterfaceUsuario

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import Titulo from "./components/Titulo.svelte";
+</script>
+
+<div class="app">
+  <header>
+    <Titulo />
+  </header>
+</div>
+
+<style>
+  .app {
+    max-height: 100vh;
+  }
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+</style>
